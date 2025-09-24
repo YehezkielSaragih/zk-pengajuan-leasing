ad2f8aea533cd020ea28b04c0ad5d865b1156c2f
Yehezkiel Stephanus Austin <yehezkiel.stephanus@gmail.com>
2025-09-24 11:16:01 +0700

    test: security

--- src/main/java/com/fif/zk/config/SecurityConfig.java
+++ src/main/java/com/fif/zk/config/SecurityConfig.java
@@ -1,45 +1,55 @@
-//package com.fif.zk.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    private final UserDetailsService userDetailsService;
-//
-//    public SecurityConfig(UserDetailsService userDetailsService){
-//        this.userDetailsService=userDetailsService;
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .authorizeRequests()
-//                    .antMatchers("/login.zul", "/register.zul", "/css/**", "/js/**", "/images/**").permitAll()
-//                    .anyRequest().authenticated()
-//                .and()
-//                    .formLogin()
-//                    .loginPage("/login.zul")
-//                    .defaultSuccessUrl("/index.zul", true)
-//                    .permitAll()
-//                .and()
-//                    .logout()
-//                    .logoutUrl("/logout")
-//                    .logoutSuccessUrl("/login.zul")
-//                    .permitAll()
-//                .and()
-//                    .exceptionHandling()
-//                    .accessDeniedPage("/accessDenied.zul");
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService);
-//    }
-//}
+package com.fif.zk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/login.zul", "/register.zul", "/css/**", "/js/**", "/images/**").permitAll()
+                    .antMatchers(
+                            "/pages/layout.zul?page=creditor-dashboard.zul",
+                            "/pages/layout.zul?page=creditor-form.zul"
+                    ).hasRole("USER")
+                    .antMatchers(
+                            "/pages/layout.zul?page=creditor-dashboard.zul",
+                            "/pages/layout.zul?page=creditor-form.zul",
+                            "/pages/layout.zul?page=loan-dashboard.zul",
+                            "/pages/layout.zul?page=loan-form.zul"
+                    ).hasRole("ADMIN")
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/login.zul")
+                    .defaultSuccessUrl("/pages/layout.zul?page=creditor-dashboard.zul", true)
+                    .permitAll()
+                .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/login.zul")
+                    .permitAll()
+                .and()
+                    .exceptionHandling()
+                    .accessDeniedPage("/pages/accessDenied.zul");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+}
